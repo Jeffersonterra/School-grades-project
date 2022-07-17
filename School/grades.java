a95d7be2e153e78e81e9218dcdbe3bbdf4061810
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-17 11:27:32 -0300

    Update grades.java

--- School/grades.java
+++ School/grades.java
@@ -4,6 +4,10 @@ import java.util.*;
 
 public class grades {
     public static void main(String[] args) {
+        Stack<String> grade = new Stack<String>();
+        Stack<String> gradeB = new Stack<String>();
+        Stack<String> gradeC = new Stack<String>();
+        Stack<String> gradeD = new Stack<String>();
 
         Stack<String> stack = new Stack<String>();
         Scanner in = new Scanner(System.in);
@@ -14,9 +18,16 @@ public class grades {
             System.out.println("Would you like add a new student? Chose 1 for yes or 0 for no : ");
             z = in.nextInt();
             // sc.nextLine();
+            System.out.println("Student name: ");
+
             String placeHolder = sc.nextLine();
             System.out.println(placeHolder);
             stack.push(placeHolder);
+            System.out.println("Student grade 1: ");
+
+            String gradeA = sc.nextLine();
+            grade.push(gradeA);
+
         } while (z != 0);
         System.out.println("The list is : ");
         // stack.pop();
