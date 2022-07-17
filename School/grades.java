a8f6f776ae39c28d559b487023ab6e484d44b0bf
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-07-16 21:42:49 -0300

    Update grades.java

--- School/grades.java
+++ School/grades.java
@@ -7,16 +7,18 @@ public class grades {
 
         Stack<String> stack = new Stack<String>();
         Scanner in = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
         String placeHold = ("");
         int z = 1;
         do {
             System.out.println("Would you like add a new student? Chose 1 for yes or 0 for no : ");
             z = in.nextInt();
-            String placeHolder = in.nextLine();
+            // sc.nextLine();
+            String placeHolder = sc.nextLine();
+            System.out.println(placeHolder);
             stack.push(placeHolder);
         } while (z != 0);
-
+        System.out.println("The list is : ");
         // stack.pop();
         Iterator<String> itr = stack.iterator();
         while (itr.hasNext()) {
